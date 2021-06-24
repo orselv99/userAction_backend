8ebc71cdfe839cb606fa4cb947a226e35b1d017a
orseL <orseLv99@gmail.com>
2021-06-24 16:56:44 +0900

    정책데이터 읽어서 클라이언트 request return

--- sources/socket/userData.js
+++ sources/socket/userData.js
@@ -2,6 +2,8 @@ module.exports = (config) => {
     const serverConfig = config.getServerConfig('tcp');
     const net = require('net');
     const server = net.createServer();
+    const fs = require('fs');
+    const path = require("path");
 
     // TODO: 저 debug 로그들 함수형태로 정리해보자
 
@@ -9,36 +11,37 @@ module.exports = (config) => {
         console.log(`${serverConfig.ip}:${serverConfig.port} is running.`);
     });
     server.on('connection', (socket) => {
-        console.log(`${Date.now()}: Someone is connected.`);
+        console.log('Someone is connected.');
         socket.on('data', (data) => {
             console.log(`${data}`);
-            
+
             try {
                 const request = JSON.parse(data);
                 switch (request.id) {
                     case 0:
-                        console.log(`${Date.now()}: request download rule.`);
-                        // TODO: 정책설정 페이지에서 만들어진 json data 를 읽어서 클라이언트에 return
-                        const result = {
-                            result: true
-                        };
-                        socket.write(JSON.stringify(result));
+                        console.log('request download rule.');
+
+                        // 생성된 최근정책을 읽어서 전달
+                        let result = fs.readFileSync(path.resolve(__dirname, 'latestRule.json'));
+                        socket.write(result);
                         break;
                     case 1:
-                        console.log(`${Date.now()}: request upload user data.'`);
+                        console.log('request upload user data.');
                         // TODO: 클라이언트에서 전달해온 데이터를 파일로 저장
                         break;
                 }
-            } 
+            }
             catch (error) {
-                console.log(`${Date.now()}: ${error}`);
+                //
+                console.log(`err: ${error}`);
+                socket.write(JSON.stringify(result));
             }
         });
         socket.on('end', () => {
-            console.log(`${Date.now()}: request end.`);
+            console.log('request end.');
         });
         socket.on('error', (error) => {
-            console.log(`${Date.now()}: ${error}`);
+            console.log(`err: ${error}`);
         });
     });
     server.on('close', (socket) => {
